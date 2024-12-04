2f20fb6f7c6a967de46f0e2261300bc4cfac63f4
Rav S <rsingh0815@gmail.com>
2024-12-03 23:40:14 -0500

    QuadTree, Google S2, GeoHash

---
+++ ARRAYS/daily_temperatures.js
@@ -0,0 +1,12 @@
+/*
+'''
+You are given a list of daily temperatures, T, where each element represents the temperature of a single day. Your task is to return a list such that, for each day in the input, it tells you how many days you would have to wait until a you reach a day with a warmer temperature. If there is no future day for which this is possible, put 0 instead. For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72], your output should be [1, 1, 0, 2, 1, 0].
+
+Example(s)
+Given the list of temperatures T = [73, 74, 75, 71, 69, 72], your output should be [1, 1, 0, 2, 1, 0].
+ 
+function dailyTemperatures(temps) {
+def dailyTemperatures(self, temps: List[int]) -> List[int]:
+
+'''
+*/
